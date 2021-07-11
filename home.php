2bc43767ec06d532f676ddcd65d1c755fcf8713d
Andriawan1 <81581236+Andriawan1@users.noreply.github.com>
2021-07-11 14:42:36 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,5 @@
+<center><br><br><br><br><br><br><br><br>
+    <img src="assets/images/logo-upb.png" width="400px height=" 400px" /> <br>
+    <font Size="9" face="Helvetica">Tehnik Informatika</font> <br>
+    <font Size="9">Universitas Pelita Bangsa</font>
+</center>
